23ea0bc2ebce0f8c7a90c4a77ba91671bbd9a151
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2023-08-03 11:56:05 -0400

    Began creating the Billform component and rendered

---
+++ src/components/Billform.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Billform() {
+  return (
+    <form>
+      <label>Bill Name:
+        <input type='text' name='bill-name' placeholder='Enter bill name'/>
+      </label>
+    </form>
+  )
+}

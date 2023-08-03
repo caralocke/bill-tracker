573f57d7feb519869a9ab9710062e83168f1076b
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2023-08-03 12:44:31 -0400

    Imported React Calendar and rendered it to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Billform from './components/Billform.js';
+import CalendarComponent from './components/CalendarComponent';
 
 function App() {
   return (
     <div className="App">
       <Billform/>
+      <CalendarComponent/>
     </div>
   );
 }

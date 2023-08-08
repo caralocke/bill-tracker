ee6755966373d9dc6c6c6573a87ce50769be095b
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2023-08-08 17:36:23 -0400

    Getting correct action.payload

--- src/components/Billform.js
+++ src/components/Billform.js
@@ -1,25 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch, connect } from 'react-redux';
 import { addBill } from '../features/billSlice';
 import { v4 as uuid} from 'uuid';
-
+import bills from '../data';
 
 const Billform = (props) => {
-  console.log('Billform props', props)
+  useEffect(() => {
+    if (props) {
+      console.log('Billform props:', props)
+    }
+  }, [props]);
+
   const [bill, setBill] = useState({
     billName: '',
     billAmount: '',
-    dueDate:''
+    dueDate:'',
+    id: ''
   });
  
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     const type = e.target.type;
     const name = e.target.name;
 
     const value = type === 'checkbox' ? e.target.checked : e.target.value
+    
 
-    setBill(prevData => ({...prevData, [name]: value}));
+    setBill(bill => ({...bill, [name]: value}));
     // setBill({
     //   ...bill,
     //   [e.target.name]: e.target.value
@@ -29,14 +37,17 @@ const Billform = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('bill', JSON.stringify(bill)); /*-----------------console.log here----------------- */
-    // console.log('bills', JSON.stringify(bills)); /*-----------------console.log here----------------- */
-    props.addBill({
-      ...bill,
-      id: uuid
-    })
+    console.log('bill to be added:', JSON.stringify(bill)); /*-----------------console.log here----------------- */
+    // props.addBill({
+    //   ...bills,
+    //   id: Date.now(),
+    // })
+    dispatch(addBill(bill));
+    console.log('all bills:', JSON.stringify(bills)); /*-----------------console.log here----------------- */
     // console.log('Billform.js bills', bills)
   }
+
+  
 
   const { billName, billAmount, dueDate } = bill;
 
@@ -51,12 +62,12 @@ const Billform = (props) => {
           </div>
           <div>
             <label>Amount:
-              <input type='text' value={billAmount }onChange={(e) => handleChange(e)} name='billAmount' id='billAmount' data-type='currency' placeholder='$0.00'/>
+              <input type='text' value={billAmount} onChange={handleChange} name='billAmount' id='billAmount' data-type='currency' placeholder='$0.00'/>
             </label>
           </div>
           <div>
             <label>Due Date:
-              <input type='date' value={dueDate} onChange={(e) => handleChange(e)} name="dueDate" id='dueDate'/>
+              <input type='date' value={dueDate} onChange={handleChange} name="dueDate" id='dueDate'/>
             </label>
           </div>
           <button>Submit</button>
@@ -64,11 +75,27 @@ const Billform = (props) => {
       </div>
       <div className='bills-container'>
         <h3>Bills</h3>
-        <ul>
-        </ul>
+        {bills.forEach(bill => {
+          return (
+            <div className='bill'>
+              Name: {billName}
+              Amount: {billAmount}
+              Due Date: {dueDate}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
 }
 
-export default connect(null, {addBill})(Billform);
+
+const mapStateToProps = (state) => {
+  console.log('App.js state', state)
+  return {
+    bills: state.bills
+  }
+}
+
+
+export default connect(mapStateToProps, {addBill})(Billform);
--- src/reducers/billReducer.js
+++ src/reducers/billReducer.js
@@ -3,6 +3,7 @@ import bills from './../data.js'
 
 const initialState = {
   bills: bills,
+  appTitle: 'Bill Tracker'
 }
 
 // const initialState = {
@@ -13,19 +14,37 @@ const initialState = {
 //   ]
 // }
 
-export default function  billReducer(state = initialState, action) {
-  console.log('action', action);
+// export default function  billReducer(state = initialState, action) {
+//   console.log('action', action);
+//   switch(action.type) {
+//     case DELETE_BILL:
+//       return {
+//         bills: state.bills.filter(item=>(action.payload !== item.id))
+//       }
+//     case ADD_BILL:
+//       return {
+//         bills: state.bills.push(action.payload)
+//       }
+//       default:
+//         return state;
+//       }
+//     }
+
+const reducer = (state = initialState, action) => { 
+  console.log('action.payload', action.payload)
   switch(action.type) {
-    case DELETE_BILL:
-      return {
-        bills: state.bills.filter(item=>(action.payload !== item.id))
-      }
-    case ADD_BILL:
-      return {
-        bills: state.bills.push(action.payload)
-      }
+      case DELETE_BILL:
+          return {
+              bills: state.bills.filter(item=>(action.payload !== item.id))
+          }   
+      case ADD_BILL:
+          return {
+              ...state,
+              bills: [ ...state.bills, action.payload ]
+          }    
       default:
-        return state;
-      }
-    }
-    console.log('ADD_BILL bills', bills)
+          return state;
+  }
+}
+
+export default reducer;

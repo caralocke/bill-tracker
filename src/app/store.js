b8b7ff19b71da621d7bd30950e93ef4eaffdb0f3
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2023-08-14 13:34:45 -0400

    Set initial state to API data

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, applyMiddleware, getDefaultMiddleware } from "@reduxjs/toolkit";
 import billReducer from '../features/billSlice';
 import { devToolsEnhancer } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
 export default configureStore({
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
   reducer: {
     bill: billReducer,
   },
--- src/components/Bills.js
+++ src/components/Bills.js
@@ -1,34 +1,20 @@
 import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, getState, useDispatch } from 'react-redux'
 import { useState } from 'react'
 import axios from 'axios';
+import { getBills } from '../features/billSlice'
 
 
 export default function Bills() {
-  
-  // const bills = useSelector(state => state.bill.bills);
-  const endpoint = 'https://api-for-bills.onrender.com/api/v1/bills'
-  const [ bills, setBills ] = useState([]);
-  const getBills = async () => {
-    try {
-      const fetchdData = await axios.get(endpoint)
-      console.log('fetchData', fetchdData)
-      setBills(fetchdData.data)
-      console.log('bills after setBills', bills)
-    } catch (error){
-      console.log(error)
-    }
-  }
+
+  const dispatch = useDispatch();
+  const bills = useSelector((state) => state.bill.bills)
+
   useEffect(() => {
-    window.addEventListener('load', getBills);
-    return () => {
-      window.removeEventListener('load', getBills);
-    };
-  },[bills]);
+    dispatch(getBills())
+  }, [])
 
-  console.log('Bills.js bills', bills)
 
-  
   return (
     <div className='bills-container'>
         <h3>Bills</h3>

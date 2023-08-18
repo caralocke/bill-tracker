6b00c92011e1a0ed289ec5aed4257522da10ff2b
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2023-08-18 13:21:55 -0400

    Trying to make even for total bills

--- src/components/CalendarComponent.js
+++ src/components/CalendarComponent.js
@@ -5,19 +5,54 @@ import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import events from '../data'
 
+const weeklyEvents = [
+  {
+    id: 0,
+    title: 'All Day Event very long title',
+    allDay: true,
+    start: new Date('2023-08-27'),
+    end: new Date('2023-08-27'),
+  },
+  {
+    id: 1,
+    title: 'Long Event',
+    start: new Date('2023-09-01'),
+    end: new Date('2023-09-01'),
+  },
+]
+
 const localizer = momentLocalizer(moment);
 
 export default function CalendarComponent() {
-  const [myEvents, setEvents] = useState(events);
-  console.log('myEvents', myEvents);
-
   const bills = useSelector((state)=>state.bill.bills);
-  console.log('calendar bills', bills);
-  const [billData, setBillData] = useState(bills);
-  console.log('billData calendar', billData);
+  const [ billData, setBillData ] = useState(bills);
+  const [ myEvents, setEvents ] = useState(events);
+  const [ weeklyEvents, setWeeklyEvents ] = useState([]);
+  const [ totalDue, setTotalDue ] = useState([]);
+  console.log('myEvents', myEvents);
+  // console.log('weekly events', weeklyEvents);
+  // console.log('billData', billData)
+  console.log('totalDue', totalDue);
+
+  const addEvent = (event) => {
+    setEvents((prevData) => [...prevData, event])
+  }
+
+  const addWeeklyEvent = (bill) => {
+    setWeeklyEvents((prevData) => [...prevData, bill])
+  }
+
+  const addTotalDue = (event) => {
+    setTotalDue(event)
+  }
+
   useEffect(() => {
     setBillData(bills)
   }, [bills]);
+
+  useEffect(() => {
+    addEvent(totalDue)
+  }, [totalDue])
 
  useEffect(() => {
   billData.forEach((bill) => {
@@ -27,9 +62,40 @@ export default function CalendarComponent() {
       start: new Date(bill.dueDate),
       end: new Date(bill.dueDate)
     }
-    setEvents((prevData) => [...prevData, newData]);
+    addEvent(newData);
   })
- },[billData])
+ },[billData]);
+
+
+ useEffect(() => {
+   billData.forEach((bill) => {
+    let date = Date.parse(bill.dueDate)
+    const start = Date.parse('2023-08-27')
+    const end = Date.parse('2023-09-02')
+    if (date >= start && date <= end){
+      addWeeklyEvent(bill)
+    }
+   })
+ }, [billData])
+
+ useEffect(() => {
+  let total = 0;
+  weeklyEvents.forEach((event) => {
+   console.log('event', event)
+   total += Number(event.billAmount);
+   console.log('total:', total)
+   let totalOfEvents ={
+    id: event.id,
+    title: `Total this week: $${total}`,
+    start: new Date(2023, 7, 27),
+    end: new Date(2023, 7, 27)
+   }
+   addTotalDue(totalOfEvents)
+  })
+ },[weeklyEvents])
+ 
+
+ 
 
 
 

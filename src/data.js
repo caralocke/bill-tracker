2017a674318fc1186b2f52586faeca47d30f87a7
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2023-08-18 03:00:11 -0400

    changed data.js to reflect events

--- src/data.js
+++ src/data.js
@@ -3,14 +3,14 @@ let events = [
     id: 0,
     title: 'All Day Event very long title',
     allDay: true,
-    start: new Date(2015, 3, 0),
-    end: new Date(2015, 3, 1),
+    start: new Date(2023, 7, 19),
+    end: new Date(2023, 7, 19),
   },
   {
     id: 1,
     title: 'Long Event',
-    start: new Date(2015, 3, 7),
-    end: new Date(2015, 3, 10),
+    start: new Date(2023, 7, 23),
+    end: new Date(2023, 7, 23),
   },
 ];
 

0244eda21aab9bb79e24cbbad2177fe6e5b3e90e
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2023-08-23 13:23:32 -0400

    Added icons for navigation and a dark mode option

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ToggleSwitch from './ToggleSwitch';
+import billing from '../styles/icons/billing.png';
+import home from '../styles/icons/Home.png';
 
 export default function Navbar() {
+
   return (
     <div className='navbar'>
       <div>
         <h3>Bill Tracker</h3>
       </div>
       <div className='links'>
-        <Link to='/' className='home-button'>Home</Link>
-        <Link to='/bills' className='bills-button'>View Bills</Link>
+        <ToggleSwitch label="Darkmode:" className='toggle-btn'/>
+        <Link to='/' className='home-button'>
+          <img src={home} alt='Home'></img>
+        </Link>
+         <Link to='/bills' className='bills-button'> 
+          <img src={billing} alt="Bills"/>
+        </Link> 
       </div>
     </div>
   )
---
+++ src/components/ToggleSwitch.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/DarkMode.css';
+import '../styles/LightMode.css';
+
+export default function ToggleSwitch({ label }) {
+
+  const [ theme, setTheme ] = useState('light');
+  const  toggleTheme = () => {
+  if (theme === 'light') {
+    setTheme('dark')
+    } else {
+    setTheme('light')
+    }
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  return (
+    <div className='toggle-container'>
+      {label} {''}
+      <div className='toggle-switch'>
+        <input type='checkbox' className='checkbox' name={label} id={label} onClick={toggleTheme}/>
+        <label className='label' htmlFor={label}>
+          <span className='inner'/>
+          <span className='switch'/>
+        </label>
+      </div>
+    </div>
+  )
+}
